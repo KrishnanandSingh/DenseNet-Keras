03ce01f20c456d6654517bf5d96108f07ceb6498
Krishnanand Singh <krishnanand91@gmail.com>
2018-06-17 00:11:19 +0530

    updated for using densenet169

--- test_inference.py
+++ test_inference.py
@@ -3,31 +3,30 @@
 import numpy as np
 from keras.optimizers import SGD
 import keras.backend as K
-
-# We only test DenseNet-121 in this script for demo purpose
-from densenet121 import DenseNet 
-
 from keras.preprocessing import image
 from keras.applications.imagenet_utils import decode_predictions
+
+# We only test DenseNet-169 in this script for demo purpose
+from densenet169 import DenseNet
 
 im = image.load_img('resources/cat.jpg', target_size=(224, 224))
 im = image.img_to_array(im)
 
-# Subtract mean pixel and multiple by scaling constant 
+# Subtract mean pixel and multiple by scaling constant
 # Reference: https://github.com/shicai/DenseNet-Caffe
-im[:,:,0] = (im[:,:,0] - 103.94) * 0.017
-im[:,:,1] = (im[:,:,1] - 116.78) * 0.017
-im[:,:,2] = (im[:,:,2] - 123.68) * 0.017
+im[:, :, 0] = (im[:, :, 0] - 103.94) * 0.017
+im[:, :, 1] = (im[:, :, 1] - 116.78) * 0.017
+im[:, :, 2] = (im[:, :, 2] - 123.68) * 0.017
 
 if K.image_dim_ordering() == 'th':
   # Transpose image dimensions (Theano uses the channels as the 1st dimension)
-  im = im.transpose((2,0,1))
+  im = im.transpose((2, 0, 1))
 
   # Use pre-trained weights for Theano backend
-  weights_path = 'imagenet_models/densenet121_weights_th.h5'
+  weights_path = 'imagenet_models/densenet169_weights_th.h5'
 else:
   # Use pre-trained weights for Tensorflow backend
-  weights_path = 'imagenet_models/densenet121_weights_tf.h5'
+  weights_path = 'imagenet_models/densenet169_weights_tf.h5'
 
 # Insert a new dimension for the batch_size
 im = np.expand_dims(im, axis=0)
@@ -36,7 +35,8 @@ im = np.expand_dims(im, axis=0)
 model = DenseNet(reduction=0.5, classes=1000, weights_path=weights_path)
 
 sgd = SGD(lr=1e-2, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=sgd, loss='categorical_crossentropy',
+              metrics=['accuracy'])
 
 out = model.predict(im)
 
